516309aba4fe4000ca9555cf26e4577c74309f1e
CodeTecnologyYT <bryan.rosas.quispe@gmail.com>
2025-10-28 23:23:49 -0500

    Initial Project

---
+++ src/main/java/pe/com/scotibank/bff/digital/factory/challenge/BffDigitalFactoryChallengeApplication.java
@@ -0,0 +1,15 @@
+package pe.com.scotibank.bff.digital.factory.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource("classpath:bff-digital-factory-challenge-v1.0.properties")
+public class BffDigitalFactoryChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BffDigitalFactoryChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/com/scotibank/bff/digital/factory/challenge/BffDigitalFactoryChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.scotibank.bff.digital.factory.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffDigitalFactoryChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
