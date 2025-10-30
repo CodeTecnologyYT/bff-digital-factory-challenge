d3465b5977506077f3a808cb2a5b29c65d0cbc51
CodeTecnologyYT <bryan.rosas.quispe@gmail.com>
2025-10-30 14:50:33 -0500

    feat: Add LoggingAspect for centralized request/response logging and integrate Spring AOP

--- src/main/java/pe/com/scotibank/bff/digital/factory/challenge/BffDigitalFactoryChallengeApplication.java
+++ src/main/java/pe/com/scotibank/bff/digital/factory/challenge/BffDigitalFactoryChallengeApplication.java
@@ -2,9 +2,11 @@ package pe.com.scotibank.bff.digital.factory.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 @PropertySource("classpath:database.properties")
 @PropertySource("classpath:bff-digital-factory-challenge-v1.0.properties")
 public class BffDigitalFactoryChallengeApplication {
