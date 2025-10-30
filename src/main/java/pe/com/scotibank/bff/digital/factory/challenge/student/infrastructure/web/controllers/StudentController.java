5b7b36ac83eb00f2f103a2c9249a9956f1d18520
CodeTecnologyYT <bryan.rosas.quispe@gmail.com>
2025-10-30 10:13:35 -0500

    feat: Integrate OpenAPI documentation with Swagger

--- src/main/java/pe/com/scotibank/bff/digital/factory/challenge/student/infrastructure/web/controllers/StudentController.java
+++ src/main/java/pe/com/scotibank/bff/digital/factory/challenge/student/infrastructure/web/controllers/StudentController.java
@@ -1,11 +1,13 @@
 package pe.com.scotibank.bff.digital.factory.challenge.student.infrastructure.web.controllers;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import pe.com.scotibank.bff.digital.factory.challenge.student.application.ICreateStudentUseCase;
 import pe.com.scotibank.bff.digital.factory.challenge.student.domain.models.requests.StudentRequest;
 import pe.com.scotibank.bff.digital.factory.challenge.student.domain.models.responses.StudentResponse;
@@ -14,6 +16,7 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("/challenge/students")
 @RequiredArgsConstructor
+@Tag(name = "Students", description = "API students")
 public class StudentController {
 
     /* createStudentUseCase. */
@@ -26,6 +29,12 @@ public class StudentController {
      * @return {@link StudentResponse}
      */
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @ApiResponse(responseCode = "201", description = "Success in creating a student")
+    @ApiResponse(responseCode = "400", description = "Error in the information to be sent",
+            content = @Content(schema = @Schema(hidden = true)))
+    @ApiResponse(responseCode = "500", description = "Unexpected error",
+            content = @Content(schema = @Schema(hidden = true)))
     public Mono<StudentResponse> createStudent(@Valid @RequestBody Mono<StudentRequest> studentRequest) {
         return studentRequest.flatMap(createStudentUseCase::handle);
     }
